9c6eac7fc5ec2b3c711e44a71847ff3bbcc5b8f3
Tony Brix <Tony@Brix.ninja>
2017-04-17 17:12:55 -0500

    switch tests to jasmine 2

---
+++ spec/find-and-replace-close-button-spec.js
@@ -0,0 +1,28 @@
+"use babel";
+/* globals atom */
+
+describe("find-and-replace-close-button", function () {
+	beforeEach(async function () {
+		const workspaceElement = atom.views.getView(atom.workspace);
+
+		jasmine.attachToDOM(workspaceElement);
+
+		const packagesActivated = Promise.all([
+				atom.packages.activatePackage("find-and-replace"),
+				atom.packages.activatePackage("find-and-replace-close-button"),
+			]);
+
+		console.debug("command dispatch", atom.commands.dispatch(workspaceElement, "find-and-replace:toggle"));
+
+		await packagesActivated;
+
+	});
+
+	it("should add a button to .project-find", function () {
+		expect(document.querySelector("atom-panel .project-find .find-and-replace-close-button")).toExist();
+	});
+
+	it("should add a button to .find-and-replace", function () {
+		expect(document.querySelector("atom-panel .find-and-replace .find-and-replace-close-button")).toExist();
+	});
+});
